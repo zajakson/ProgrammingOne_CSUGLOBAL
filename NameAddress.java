9a320ca331ac19af5509266b10e3799f4f4c3aaa
IsaiahEricJackson <isaiahericjackson@gmail.com>
2024-06-16 22:48:41 -0400

    Current complete changes to NameAddress 6_16_24

---
+++ NameAddress.java
@@ -0,0 +1,22 @@
+
+public class NameAddress {
+
+    public static void main(String[] args) {
+
+        //Global Name and Address information
+        String firstName = "Isaiah";
+        String lastName = "Jackson";
+        String streetAddress = "21 WereJammin Ave";
+        String city = "Wilmington";
+        String state = "DE";
+        String zipCode = "19804";
+
+        //Print Statements 
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(state);
+        System.out.println(zipCode);
+    }
+}
